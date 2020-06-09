9dfe8b328830384fbf72ae17be0887c343cabba0
Smitkumar Patel <30504086+Patel-Smit@users.noreply.github.com>
2020-06-09 18:45:41 -0400

    LoginActivity, Signup design DONE

--- app/src/main/java/com/smit/cegepe_learning/LoginActivity.java
+++ app/src/main/java/com/smit/cegepe_learning/LoginActivity.java
@@ -2,13 +2,38 @@ package com.smit.cegepe_learning;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
+
+    TextView signUp, forgotPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        signUp = findViewById(R.id.login_tv_signUP);
+        forgotPassword = findViewById(R.id.login_tv_forgotPassword);
+
+        signUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(LoginActivity.this, SignupActivity.class);
+                startActivity(i);
+            }
+        });
+
+        forgotPassword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(LoginActivity.this, MainActivity.class);
+                startActivity(i);
+            }
+        });
     }
 }

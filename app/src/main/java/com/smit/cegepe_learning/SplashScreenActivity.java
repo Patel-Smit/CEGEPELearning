e36812eeb888df80b5e52498b0974841f22cdd4a
Smitkumar Patel <30504086+Patel-Smit@users.noreply.github.com>
2020-06-09 00:28:10 -0400

    Splash Screen Done

---
+++ app/src/main/java/com/smit/cegepe_learning/SplashScreenActivity.java
@@ -0,0 +1,50 @@
+package com.smit.cegepe_learning;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    Animation topAnim, bottomAnim;
+    ImageView logo;
+    TextView appName, appMission;
+
+    private static int SPLASH_SCREEN_TIME = 3000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_splash_screen);
+
+        //Animations
+        topAnim = AnimationUtils.loadAnimation(this, R.anim.top_animation);
+        bottomAnim = AnimationUtils.loadAnimation(this, R.anim.bottom_animation);
+
+        //Hooks
+        logo = findViewById(R.id.sp_iv_logo);
+        appName = findViewById(R.id.sp_tv_appname);
+        appMission = findViewById(R.id.sp_tv_missionstmt);
+
+        logo.setAnimation(topAnim);
+        appName.setAnimation(bottomAnim);
+        appMission.setAnimation(bottomAnim);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(SplashScreenActivity.this, MainActivity.class);
+                startActivity(i);
+                finish();
+            }
+        }, SPLASH_SCREEN_TIME);
+    }
+}
